f45811f72d56a1e864210a568c1b03fc8c19d0d2
Michael <michaelpspore@gmail.com>
2021-04-12 14:19:39 -0500

    indexed product item updated

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
-
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { cart } = state
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find(cartItem => cartItem._id === _id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
@@ -39,6 +32,8 @@ function ProductItem(item) {
     }
   }
 
+  const { image, name, _id, price, quantity } = item;
+  
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
